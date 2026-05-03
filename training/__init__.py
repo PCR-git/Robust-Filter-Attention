db0960f78a8b6a7d8b276dc639f1f7f8d4bee39f
Peter Racioppo <pracioppo@dohenyeyeinstitute.org>
2026-05-03 12:28:52 -0700

    Clear all files

--- training/__init__.py
+++
@@ -1,3 +0,0 @@
-from .train_utils_dynamics import single_epoch_rfa_dynamics, single_epoch_attn_dynamics
-from .train_utils_dynamics import hook_fn
-from .train_utils_lm import single_epoch_rfa_lm, single_epoch_standard_lm
